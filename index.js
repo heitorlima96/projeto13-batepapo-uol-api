1c05b50dc91ff2278ae9a8f69c535aac89c9e174
heitorlima96 <limaheitor.1996@gmail.com>
2022-11-14 12:54:56 -0300

    get and post messages

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import joi from 'joi';
 import cors from 'cors';
-import { MongoClient, MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 import dayjs from 'dayjs';
 
@@ -73,7 +73,67 @@ app.get('/participants', async (req, res) => {
     }
 })
 
+app.post('/messages', async (req, res) => {
+    const message = req.body;
+    const { user } = req.headers;
 
+    const messageSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().valid('message', 'private_message')
+    });
+
+    const { error } = messageSchema.validate(message);
+
+    if (error) {
+        return res.sendStatus(422);
+    }
+
+    try {
+        const participant = await db.collection('participants').findOne({ name: user });
+        if (!participant) {
+            return res.sendStatus(422);
+        }
+        
+        const {to, text, type } = message;
+        await db.collection('messages').insertOne({
+            to,
+            text,
+            type,
+            from: user,
+            time: dayjs().format('HH:mm:ss')});
+
+            res.sendStatus(201);
+    } catch (error) {
+        return res.status(500).send('Erro ao tentar salvar a mensagem');
+    }
+});
+
+app.get('/messages', async (req, res) => {
+    const limit = parseInt(req.query.limit);
+    const { user } = req.headers;
+  
+    try {
+        const messages = await db.collection("messages").find().toArray();
+        const filterMessages = messages.filter(message => {
+            const { from, to, type } = messages;
+            const toUser = to === "Todes" || (to === user || from === user);
+            const isPublic = type === "message";
+
+            return toUser || isPublic;
+        });
+
+        if (limit && limit === NaN) {
+            return res.send(filterMessages.slice(-limit));
+        }
+
+        res.send(filterMessages);
+        
+    } catch (error) {
+        console.log('Erro ao tentar obter mensagens', error);
+        res.sendStatus(500);
+    }
+});
 
 
 const PORT = process.env.PORT || 5000;
