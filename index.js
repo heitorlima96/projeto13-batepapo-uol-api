a82bdf819daba563b689cec3e8c35c7a3b9e0282
heitorlima96 <limaheitor.1996@gmail.com>
2022-11-14 13:24:29 -0300

    post status

--- index.js
+++ index.js
@@ -135,6 +135,23 @@ app.get('/messages', async (req, res) => {
     }
 });
 
+app.post('/status', async (req, res) => {
+    const { user } = req.headers;
+    try {
+        const participant = await db.collection('participants').findOne({ name: user });
+        if (!participant) return res.sendStatus(404);
+
+        await db.collection('participants').updateOne({ name: user }, { $set: { lastStatus: Date.now() } });
+        res.sendStatus(200);
+        
+    } catch (error) {
+        console.log('Erro ao tentar atualizar o status', error);
+        res.sendStatus(500);
+        
+    }
+})
+
+
 
 const PORT = process.env.PORT || 5000;
 
